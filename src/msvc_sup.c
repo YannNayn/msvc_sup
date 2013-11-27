9cee06efa0e76e95d5d8ac13d71495aae7dd3a66
YourName@a.a <you@example.com>
2013-11-27 13:39:54 +0100

    .

--- src/msvc_sup.c
+++ src/msvc_sup.c
@@ -102,17 +102,31 @@ static const unsigned __int64 epoch = 11644473600000000Ui64;
  */
 int
 gettimeofday(struct timeval * tp, struct timezone * tzp) {
-  FILETIME file_time;
-  SYSTEMTIME system_time;
-  ULARGE_INTEGER ularge;
+	FILETIME file_time;
+	SYSTEMTIME system_time;
+	ULARGE_INTEGER ularge;
+	static int tzflag;
+	if (NULL != tv)
+	{
 
-  GetSystemTime(&system_time);
-  SystemTimeToFileTime(&system_time, &file_time);
-  ularge.LowPart = file_time.dwLowDateTime;
-  ularge.HighPart = file_time.dwHighDateTime;
+		  GetSystemTime(&system_time);
+		  SystemTimeToFileTime(&system_time, &file_time);
+		  ularge.LowPart = file_time.dwLowDateTime;
+		  ularge.HighPart = file_time.dwHighDateTime;
 
-  tp->tv_sec = (long)((ularge.QuadPart - epoch) / 10000000L);
-  tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+		  tp->tv_sec = (long)((ularge.QuadPart - epoch) / 10000000L);
+		  tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+	}
+	if (NULL != tzp)
+	{
+		if (!tzflag)
+		{
+			_tzset();
+			tzflag++;
+		}
+		tzp->tz_minuteswest = _timezone / 60;
+		tzp->tz_dsttime = _daylight;
+	}
 
   return 0;
 }
